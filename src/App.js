b45db722f52ffebacb6bae0c59a0e593db913b49
themostluckytina <themostluckytina@gmail.com>
2022-04-09 22:17:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import { data } from './data';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [ item, setItem ] = useState(data);
+
+  const removeItem = (id) =>{
+    let newList = item.filter(toDo =>
+      toDo.id !== id);
+      setItem(newList);
+
+  }
+
+
+
+  return ( <div>
+    <div className="container">
+      <h1>ПРЕДСВАДЕБНЫЕ ХЛОПОТЫ</h1>
     </div>
+
+    <div className='list'>
+      {item.map((thing =>{
+        const{id, toDo} = thing;
+
+        return(<div  key={id}>
+          <div className='todo'>
+          <h4>{id}. {toDo} </h4>
+          <button onClick={()=>removeItem(id)}  className='heart'>V</button>
+          </div>
+
+        </div>)
+
+
+
+      }))}
+
+
+
+    </div>
+
+
+
+    <div className="container">
+      <button className='btn' onClick={()=>setItem([])}> Удалить весь список</button>
+    </div>
+    <div className="container"></div>
+    <div className="container"></div>
+    <div className="container"></div>
+
+
+
+
+
+</div>
   );
 }
 
